b0367b922aef62795ccbc6120d466946b8b3a6a0
theyusuph <ybedelci@gmail.com>
2023-06-29 15:02:01 +0200

    chapter 1

---
+++ myblogsite/blog/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.db.models.query import QuerySet
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+# Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self) -> QuerySet:
+        return super().get_queryset()\
+                      .filter(status=Post.Status.PUBLISHED)
+
+
+class Post(models.Model):
+
+    # The enumartion class below can be interacted with in the following ways:
+    # Post.Status
+    # Post.Status.choices -> (DF, Draft), (PB, Published)
+    # Post.Status.values -> Draft, Published
+    # Post.Status.DRAFT.label -> DF
+    # Post.Status.DRAFT.choice -> Draft
+    class Status(models.TextChoices):
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name="blog_posts")
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.DRAFT)
+
+    objects = models.Manager()
+    published = PublishedManager()
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish'])
+        ]
+
+    def __str__(self):
+        return self.title
