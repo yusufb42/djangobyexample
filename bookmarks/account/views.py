1cf48122684b20227ea1608d156962c7c9c30ea2
theyusuph <ybedelci@gmail.com>
2023-07-16 23:50:12 +0200

    chapter 4

---
+++ bookmarks/account/views.py
@@ -0,0 +1,100 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+from .models import Profile
+
+
+@login_required
+def dashboard(request):
+    return render(
+        request,
+        "account/dashboard.html",
+        {
+            "section": "dashboard",
+        },
+    )
+
+
+@login_required
+def edit(request):
+    if request.method == "POST":
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(
+            instance=request.user.profile, data=request.POST, files=request.FILES
+        )
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, "profile updated successfully")
+
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+        messages.error(request, "error updating your profile")
+
+    return render(
+        request,
+        "account/edit.html",
+        {
+            "user_form": user_form,
+            "profile_form": profile_form,
+        },
+    )
+
+
+def user_login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(
+                request,
+                username=cd["username"],
+                password=cd["password"],
+            )
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse("authenticated successfully")
+                else:
+                    return HttpResponse("disabled account")
+            else:
+                return HttpResponse("invalid login")
+    else:
+        form = LoginForm()
+
+    return render(
+        request,
+        "account/login.html",
+        {
+            "form": form,
+        },
+    )
+
+
+def register(request):
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # set the chosen password
+            new_user.set_password(user_form.cleaned_data["password"])
+            # save the User object
+            new_user.save()
+
+            # create the user profile
+            Profile.objects.create(user=new_user)
+
+            return render(request, "account/register_done.html", {"new_user": new_user})
+
+    else:
+        user_form = UserRegistrationForm()
+
+    return render(request, "account/register.html", {"user_form": user_form})
