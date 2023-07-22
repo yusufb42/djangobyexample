0df052b50bcb8662f9608030ac78ac1acf136f54
theyusuph <ybedelci@gmail.com>
2023-07-22 11:18:20 +0200

    chapter 8 (and start of 9)

---
+++ myshop/orders/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+from django.shortcuts import reverse
+
+from .models import OrderItem
+from .forms import OrderCreateForm
+from cart.cart import Cart
+from .tasks import order_created
+
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == "POST":
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order,
+                    product=item["product"],
+                    price=item["price"],
+                    quantity=item["quantity"],
+                )
+
+            # clear the cart
+            cart.clear()
+
+            # launch asynchronous task
+            order_created.delay(order.id)
+
+            # set the order in the session
+            request.session["order_id"] = order.id
+
+            # redirect for payment
+            # return redirect
+            return render(request, "orders/order/created.html", {"order": order})
+    else:
+        form = OrderCreateForm()
+
+    return render(request, "orders/order/create.html", {"cart": cart, "form": form})
